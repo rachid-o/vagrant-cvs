9d22d2557dd3977f00bd67a720748855f004b66f
Rachid Ben Moussa <Rachid.BenMoussa@planonsoftware.com>
2015-04-20 09:13:40 +0200

    Refactored scripts to be more readable and added logging of progress

--- homedir/migrate-cvs2git.sh
+++ homedir/migrate-cvs2git.sh
@@ -2,19 +2,29 @@
 
 [ "$1" == "" ] && echo -e "Usage: migrate [PROJECTNAME] \n[PROJECTNAME] is the name of the module in CVS" &&  exit;
 PROJECT=$1
+LOGFILE=$PROJECT-cvs2git.log
 
-##
-time cvs2git --trunk-only --fallback-encoding=utf8 --blobfile=blob-$PROJECT.dat \
-	--dumpfile=dump-$PROJECT.dat --username=cvs2git /cvs-repo/$PROJECT 
+echo "Output will be logged to: $LOGFILE"
+
+#CVSROOT=/cvs-repo
+CVSROOT=~/cvs-repo2
+time cvs2git -q --trunk-only --fallback-encoding=utf8 --blobfile=blob-$PROJECT.dat \
+	--dumpfile=dump-$PROJECT.dat --username=cvs2git $CVSROOT/$PROJECT &>  $LOGFILE 
+
+EXIT_CODE=$?
+if [ ! $EXIT_CODE -eq 0 ]; then
+	echo "Error; cvs2git returned with code $EXIT_CODE" >&2
+	exit $EXIT_CODE;
+fi
 
 mkdir $PROJECT.git.bare
 cd $PROJECT.git.bare
 git init --bare
 
-## Import the data into Git
-time cat ../blob-$PROJECT.dat ../dump-$PROJECT.dat | git fast-import
+echo "Import the data into Git repo"
+time cat ../blob-$PROJECT.dat ../dump-$PROJECT.dat | git fast-import  >> $LOGFILE
 
 ## Cleanup tmp files
-rm *$PROJECT.dat
+#rm *$PROJECT.dat
 
 echo "Done migrating to Git. You should verify the outcome!"
--- homedir/verify-cvs2git.sh
+++ homedir/verify-cvs2git.sh
@@ -3,40 +3,47 @@
 ## ==  Verify migration  ==
 ## ========================
 
-[ "$1" == "" ] && echo -e "Usage: migrate [PROJECTNAME] \n[PROJECTNAME] is the name of the module in CVS" &&  exit;
+[ "$1" == "" ] && echo -e "Usage: verify [PROJECTNAME] \n[PROJECTNAME] is the name of the module in CVS. \n\nThis script verifies the outcome of migrate-cvs2git.sh so assumes there is a Git repo in the dir PROJECTNAME.git.bare" &&  exit;
 PROJECT=$1
+PROJECT_CVS=$PROJECT.cvs.diff
+PROJECT_GIT=$PROJECT.git.diff
 
+## Copy CVS repo to local path because due to permissions we can not checkout directly
 #cp -r /cvs-repo/$PROJECT ~/cvs-repo2/
-## Copied to local path because doe to permissions 
 export CVSROOT=~/cvs-repo2
 
 if [ ! -d "$PROJECT" ]; then
-	cvs co $PROJECT
+	echo Checking out $PROJECT from cvs
+	cvs co $PROJECT  &> /dev/null 
 fi
-rm -r $PROJECT.cvs 
-cp -r $PROJECT $PROJECT.cvs
- 
-# Delete files we can ignore during comparison
-find $PROJECT.cvs -type d -name CVS -exec rm -r {} \; &> /dev/null 
-# Git does not support empty directories
-find $PROJECT.cvs -type d -empty -delete &> /dev/null 
- 
-## Clone git project
-rm -r $PROJECT.git
-git clone $PROJECT.git.bare $PROJECT.git &> /dev/null 
-rm -r $PROJECT.git/.git
+
+
+if [ ! -d "$PROJECT_CVS" ]; then
+	echo "Copying CVS project and remove files which we ignore during comparison"
+	cp -r $PROJECT $PROJECT_CVS
+	## Delete files we can ignore during comparison
+	find $PROJECT_CVS -type d -name CVS -exec rm -r {} \; &> /dev/null
+	## Git does not support empty directories
+	find $PROJECT_CVS -type d -empty -delete &> /dev/null 
+fi
+
+## Clone bare git project
+if [ ! -d "$PROJECT_GIT" ]; then
+	echo "Cloning bare git project"
+	git clone $PROJECT.git.bare $PROJECT_GIT &> /dev/null 
+fi
  
 ## Compare
  
 # show files only
-diff -rq $PROJECT.git $PROJECT.cvs
+diff -rq --exclude=".git" --exclude="CVS" $PROJECT_GIT $PROJECT_CVS
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
   echo "Git repo is equal to the CVS repo \o/"
 else
   echo "Differences between repo's!"
   echo "For detailed differences do: "
-  echo "diff -r $PROJECT.git $PROJECT.cvs"
+  echo "diff -r --exclude='.git' --exclude='CVS' $PROJECT_GIT $PROJECT_CVS"
 fi
-# Show all differences detailed 
-#diff -r $PROJECT.git $PROJECT.cvs
+
+echo "   cleanup with: rm -r $PROJECT $PROJECT_CVS $PROJECT_GIT"
