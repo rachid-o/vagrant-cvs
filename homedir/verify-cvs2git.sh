a1e367e8faf053750ab7341c917dd2ccf4738f82
Rachid Ben Moussa <Rachid.BenMoussa@planonsoftware.com>
2015-04-21 14:59:05 +0200

    Ignore CVS revision in comments when comparing the repo's

--- homedir/verify-cvs2git.sh
+++ homedir/verify-cvs2git.sh
@@ -7,16 +7,17 @@
 PROJECT=$1
 PROJECT_CVS=$PROJECT.cvs.diff
 PROJECT_GIT=$PROJECT.git.diff
+export CVSROOT=~/cvs-repo2
 
 ## Copy CVS repo to local path because due to permissions we can not checkout directly
-#cp -r /cvs-repo/$PROJECT ~/cvs-repo2/
-export CVSROOT=~/cvs-repo2
+#if [ ! -d "$CVSROOT/$PROJECT" ]; then
+#	cp -r /cvs-repo/$PROJECT $CVSROOT/
+#fi
 
 if [ ! -d "$PROJECT" ]; then
 	echo Checking out $PROJECT from cvs
 	cvs co $PROJECT  &> /dev/null 
 fi
-
 
 if [ ! -d "$PROJECT_CVS" ]; then
 	echo "Copying CVS project and remove files which we ignore during comparison"
@@ -29,21 +30,27 @@ fi
 
 ## Clone bare git project
 if [ ! -d "$PROJECT_GIT" ]; then
-	echo "Cloning bare git project"
+	echo "Cloning bare Git project"
 	git clone $PROJECT.git.bare $PROJECT_GIT &> /dev/null 
 fi
  
 ## Compare
  
+
+#IGNORE_PARAMS=" --ignore-matching-lines=' \* @version.*\$Revision' --ignore-matching-lines='\*.*\$Id' "
+#echo "IGNORE_PARAMS: $IGNORE_PARAMS"
+
 # show files only
-diff -rq --exclude=".git" --exclude="CVS" $PROJECT_GIT $PROJECT_CVS
+diff -rq --exclude=".git" \
+	--ignore-matching-lines=" * @version.*\\$\Revision" --ignore-matching-lines="* \\$\Id" \
+	$PROJECT_GIT $PROJECT_CVS
+
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
   echo "Git repo is equal to the CVS repo! \o/"
 else
-  echo "Differences between repo's!"
-  echo "For detailed differences do: "
-  echo "diff -r --exclude='.git' --exclude='CVS' $PROJECT_GIT $PROJECT_CVS"
+  echo -e "Differences between repo's! \nFor detailed differences do: "
+  echo "diff -r --exclude='.git' --ignore-matching-lines=\" * @version.*\\$\Revision\" --ignore-matching-lines=\"* \\$\Id\" $PROJECT_GIT $PROJECT_CVS"
 fi
 
-echo "   cleanup with: rm -r *.dat $PROJECT $PROJECT_CVS $PROJECT_GIT"
+echo -e "To cleanup perform: \nrm -r *.dat $PROJECT $PROJECT_CVS $PROJECT_GIT"
